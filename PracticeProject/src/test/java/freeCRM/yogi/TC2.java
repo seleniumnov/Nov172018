f77e347383032daf80cee26797e1c378ab8c5e6a
seleniumnov <seleniumnov10@gmail.com>
2018-12-14 23:32:09 +0530

    h

--- PracticeProject/src/test/java/freeCRM/yogi/TC2.java
+++ PracticeProject/src/test/java/freeCRM/yogi/TC2.java
@@ -11,75 +11,84 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TC2 {
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver", "D:\\selenium softwares\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		// System.setProperty("webdriver.chrome.driver", "D:\\selenium
+		// softwares\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.freecrm.com");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		Thread.sleep(3000);
 		driver.manage().window().maximize();
-		
-		WebElement username=driver.findElement(By.xpath("//input[@name='username']"));
+
+		WebElement username = driver.findElement(By.xpath("//input[@name='username']"));
 		username.sendKeys("jagadeesh232708");
-		WebElement password=driver.findElement(By.xpath("//input[@name='password']"));
+		WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
 		password.sendKeys("jagadeeshjaggu");
-		WebElement login=driver.findElement(By.xpath("//input[@value='Login']"));
+		WebElement login = driver.findElement(By.xpath("//input[@value='Login']"));
 		login.click();
 		driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='mainpanel']")));
-		
+
 		Actions action = new Actions(driver);
 		WebElement we = driver.findElement(By.xpath("//a[@title='Companies']"));
 		action.moveToElement(we).build().perform();
-		List<WebElement>  searchelements=driver.findElements(By.xpath("//div[@id='navmenu']/ul/li[3]/ul/li"));
+		List<WebElement> searchelements = driver.findElements(By.xpath("//div[@id='navmenu']/ul/li[3]/ul/li"));
+		for (WebElement webElement : searchelements) {
+			String txt = webElement.getText();
+			System.out.println(txt);
+			if (txt.equals("New Company")) {
+				webElement.click();
+				break;
+			}
+		}
+
+		List<WebElement> menuItems = driver.findElements(By.xpath("(//form[@id='companyForm']/table/tbody/tr/td//*[@value='?'])"));
 		
-		for(int i=0;i<searchelements.size();i++)
-		{
-			String txt = searchelements.get(i).getText();
-			 System.out.println(txt);
-			 if(txt.equals("New Company"))
-			 {
-				 searchelements.get(i).click();
-				 break;
-			 }
-			 	
-		}
-		List<WebElement> menuItems=driver.findElements(By.xpath("//input[@value='?']"));
-		for(int j=0;j<menuItems.size();j++)
-		{
-			String homepage=driver.getWindowHandle();
-//			System.out.println(homepage);
-			menuItems.get(j).click();
-			Set<String> windows=driver.getWindowHandles();
-		    System.out.println(windows.size());
-		    Iterator iterator=windows.iterator();
-		    String currentwindowid;
-		    while(iterator.hasNext())
-		    {
-		    	currentwindowid=iterator.next().toString();
-		    	if(!currentwindowid.equals(homepage))
-				{
-		    		driver.switchTo().window(currentwindowid);
-		    		System.out.println(currentwindowid);
-		    		//driver.close();
-		    		driver.manage().window().maximize();
-		    		String title = driver.getTitle();
-		    		System.out.println(title);
-		    		String name=driver.findElement(By.xpath("//td[@class='datacardtitle']//span")).getText();
-		    		
-		    		//searchtxt.sendKeys("test");
-		    		 String txt12= driver.findElement(By.xpath("//*[contains(text(),'"+name+"')]/following::td[2]")).getText();
-    				 System.out.println(txt12);
-    				 driver.close();
-		    		 //String txt11= driver.findElement(By.xpath("//*[contains(text(),'"+txt1+"')]/following::p[1]")).getText();
-    				 //System.out.println(txt11);		    		
+		String homepage = driver.getWindowHandle();
+		
+		for (int i = 1; i <=menuItems.size(); i++) {
+			
+			
+			Thread.sleep(5000);
+			WebElement webElement = driver.findElement(By.xpath("(//form[@id='companyForm']/table/tbody/tr/td//*[@value='?'])["+i+"]"));
+
+			try {
+				webElement.click();
+				Set<String> windows = driver.getWindowHandles();
+				System.out.println(windows.size());
+				Iterator<String> iterator = windows.iterator();
+				String currentwindowid;
+				while (iterator.hasNext()) {
+					currentwindowid = iterator.next().toString();
+					if (!currentwindowid.equals(homepage)) {
+						driver.switchTo().window(currentwindowid);
+						System.out.println(currentwindowid);
+						// driver.close();
+						driver.manage().window().maximize();
+						String title = driver.getTitle();
+						System.out.println(title);
+						String name = driver.findElement(By.xpath("//td[@class='datacardtitle']//span")).getText();
+
+						// searchtxt.sendKeys("test");
+						String txt12 = driver
+								.findElement(By.xpath("//*[contains(text(),'" + name + "')]/following::td[2]"))
+								.getText();
+						System.out.println(txt12);
+						driver.close();
+						// String txt11=
+						// driver.findElement(By.xpath("//*[contains(text(),'"+txt1+"')]/following::p[1]")).getText();
+						// System.out.println(txt11);
+					}
 				}
-		    }
-			driver.switchTo().window(homepage);		 
-			 	
+				driver.switchTo().window(homepage);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
 		}
-
 	}
-
 }
